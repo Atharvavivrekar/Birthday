3d0e216cd4d783a97633e00ddb16d6d5508734c4
Atharvavivrekar <atharvavivrekar@gmail.com>
2025-02-13 17:58:06 +0100

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,27 @@
+import pygame
+import time
+
+pygame.init()
+
+
+HEIGHT = 800
+WIDTH = 800
+
+display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Birthday Greeting Card")
+
+img = pygame.image.load("background1.jpeg")
+
+
+
+while (True):
+    font = pygame.font.SysFont("Times New Roman", 72)
+    text = font.render("Happy",True,(0,0,0))
+    text2 = font.render("Birthday", True, (0, 0, 0))
+    display_surface.fill((255,255,255))
+    display_surface.blit(image,(0,0))
+    display_surface.blit(text,(210,180))
+    display_surface.blit(text2,(180,264))
+    pygame.display.update()
+    time.sleep(2)
+
